c3a17f6fad1f2ad1f8aa6fec63cbedab3137ba29
Syamiera Hani <syamierahani@gmail.com>
2025-10-30 12:50:44 +0800

    Add new stack item and list blade component

---
+++ resources/views/components/stack-item.blade.php
@@ -0,0 +1,25 @@
+@props([
+    'image' => null, 
+    'title', 
+    'subtitle' => null, 
+    'description' => null, 
+    'footer' => null, 
+    'link' => '#' // default if no link provided
+])
+
+<li class="flex justify-between gap-x-6 py-5">
+  <a href="{{ $link }}" class="flex min-w-0 gap-x-4 w-full hover:bg-gray-50 rounded-md transition">
+    <img src="{{ $image ?? 'https://via.placeholder.com/150' }}" 
+         class="size-12 flex-none rounded-full bg-gray-100" />
+
+    <div class="min-w-0 flex-auto">
+      <p class="text-sm/6 font-semibold text-gray-900">{{ $title }}</p>
+      <p class="mt-1 truncate text-xs/5 text-gray-500">{{ $subtitle }}</p>
+    </div>
+
+    <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+      <p class="text-sm/6 text-gray-900">{{ $description }}</p>
+      <p class="mt-1 text-xs/5 text-gray-500">{{ $footer }}</p>
+    </div>
+  </a>
+</li>
