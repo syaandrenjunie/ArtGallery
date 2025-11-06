2a1dfc23c7718953b70b5a0a5de3551d9f2c6acf
Syamiera Hani <syamierahani@gmail.com>
2025-11-06 14:38:00 +0800

    Configure Spatie permission package and register middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        
+        //register alias
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
     })
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
