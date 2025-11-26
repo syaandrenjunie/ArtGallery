add7c0b213fe73c7348df24b8bffdaeb75bc9394
Syamiera Hani <syamierahani@gmail.com>
2025-11-26 15:23:32 +0800

    Create LW components with Forms

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -6,8 +6,12 @@ use Livewire\Component;
 
 class Greeter extends Component
 {
-    public $name = 'John';
-    
+    public $name = 'Renjun';
+
+    public function changeName($newName) {
+        $this->name = $newName;
+    }
+
     public function render()
     {
         return view('livewire.greeter');
